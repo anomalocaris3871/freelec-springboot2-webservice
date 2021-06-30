b935da16fed76cd80ef42ee62d7173475845ab8a
Lee <anomalocaris1@naver.com>
2021-06-30 19:59:55 +0900

    2021-06-30 page78

---
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_test() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+}
